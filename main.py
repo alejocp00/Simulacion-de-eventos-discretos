b8496a9f3d727c3c3591afaeffe9f0d0f8f5bb46
alejocp00 <alex00cu@gmail.com>
2024-02-27 14:30:53 -0500

    Refactor main function to use default Simulator constructor

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from src.code.simulator import Simulator
 
 def main():
-    simulator = Simulator(n=10,s=10,i=10,random_values=False)
+    simulator = Simulator()
     simulator.run()
     simulator.show_results()
     
 main()
+
+
+
+
 
 # Todo: Permitir correr el programa con argumentos
