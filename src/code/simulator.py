608f307df8b6e68f4138a807f3ba1cb7203731a1
alejocp00 <alex00cu@gmail.com>
2024-02-27 11:31:01 -0500

    Add Simulator class to handle factory simulation

---
+++ src/code/simulator.py
@@ -0,0 +1,28 @@
+import random
+
+from src.code.factory import Factory
+
+
+class Simulator:
+    def __init__(self,i:int =0,n:int =0,s:int =0,random_values:bool = True):
+        self.__iterations = i
+        self.__n = n
+        self.__s = s
+        self.__random_values = random_values
+        self.__results = []
+
+        if self.__random_values:
+            self.__n = random.randint(10,50)
+            self.__s = random.randint(10,50)
+            self.__iterations = random.randint(1000,10000)
+            
+    def run(self):
+        for i in range(self.__iterations):
+            factory = Factory(self.__n,self.__s)
+            factory.start_factory()
+            factory_time = factory.get_factory_time()
+            factory_data = factory.get_data()
+            self.__results.append((factory_time,factory_data))
+            
+    def get_results(self):
+        return self.__results
