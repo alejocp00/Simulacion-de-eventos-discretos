b17c92821d141657e97601cf7684bfd4950a70e9
alejocp00 <alex00cu@gmail.com>
2024-02-27 14:11:31 -0500

    Refactor auxiliary functions to use random module

--- src/code/auxiliar_functions.py
+++ src/code/auxiliar_functions.py
@@ -1,10 +1,11 @@
+import random
 from scipy.stats import expon, norm
 
 def get_working_time():
     # Utiliza una distribución exponencial para el tiempo de reparación (G(t))
-    return expon.rvs()
+    return random.expovariate(1)
 
 def get_repair_time():
     # Utiliza una distribución normal para el tiempo de falla (F(t))
-    return norm.rvs()
+    return random.normalvariate(1,0.2)
 
