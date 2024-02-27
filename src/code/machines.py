a0c1b0ab5a9d53e628b9a65cf8bbf109f4d99ceb
alejocp00 <alex00cu@gmail.com>
2024-02-27 01:51:43 -0500

    Add Machine class to manage machines and their work time

---
+++ src/code/machines.py
@@ -0,0 +1,35 @@
+import time
+from src.code.auxiliar_functions import  get_working_time
+
+class Machine:
+    def __init__(self,id,work_time=get_working_time()):
+        self.__id = id
+        self.__work_time = work_time
+        self.__start_time = 0
+        
+    def get_id(self):
+        return self.__id
+    
+    def get_work_time(self) -> float:
+        return self.__work_time
+    
+    def get_start_time(self) -> float:
+        return self.__start_time
+    
+    def start_working(self):
+        self.__start_time = time.time()
+        
+    def repair(self):
+        self.__work_time = get_working_time()
+        
+    def __lt__(self, other):
+        if isinstance(other, Machine):
+            return self.__work_time < other.__work_time
+        
+    def __eq__(self, other):
+        if isinstance(other, Machine):
+            return self.__work_time == other.__work_time
+        return False
+    
+    
+    
