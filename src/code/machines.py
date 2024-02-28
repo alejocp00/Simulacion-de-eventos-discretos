9a2cf97c66894b87222bc9c15bcfe502ff9edf92
alejocp00 <alex00cu@gmail.com>
2024-02-28 17:51:03 -0500

    Refactor Machine class to include repair time

--- src/code/machines.py
+++ src/code/machines.py
@@ -1,10 +1,11 @@
 import time
-from src.code.auxiliar_functions import  get_working_time
+from src.code.auxiliar_functions import  get_repair_time, get_working_time
 
 class Machine:
-    def __init__(self,id,work_time=0):
+    def __init__(self,id,work_time=-1,repair_time=-1):
         self.__id = id
-        self.__work_time = work_time if not work_time == 0 else get_working_time()
+        self.__work_time = work_time if not work_time == -1 else get_working_time()
+        self.__repair_time = repair_time if not repair_time == -1 else get_repair_time()
         self.__start_time = 0
         
     def get_id(self):
@@ -12,6 +13,9 @@ class Machine:
     
     def get_work_time(self) -> float:
         return self.__work_time
+    
+    def get_repair_time(self) -> float:
+        return self.__repair_time
     
     def get_start_time(self) -> float:
         return self.__start_time
@@ -21,6 +25,7 @@ class Machine:
         
     def repair(self):
         self.__work_time = get_working_time()
+        self.__repair_time = get_repair_time()
         
     def __lt__(self, other):
         if isinstance(other, Machine):
