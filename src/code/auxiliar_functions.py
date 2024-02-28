2a07ecab8ec50c7e9160d6f288b12801f00b5037
alejocp00 <alex00cu@gmail.com>
2024-02-28 18:51:00 -0500

    Refactor auxiliary functions and simulator class

--- src/code/auxiliar_functions.py
+++ src/code/auxiliar_functions.py
@@ -1,11 +1,14 @@
 import random
 from scipy.stats import expon, norm
 
-def get_working_time():
+var_lambd = 1
+var_mu = 1
+var_sigma = 0.5
+
+def get_working_time(lambd:float = var_lambd):
     # Utiliza una distribución exponencial para el tiempo de reparación (G(t))
-    return random.expovariate(1)
+    return random.expovariate(lambd)
 
-def get_repair_time():
+def get_repair_time(mu:float = var_mu, sigma:float = var_sigma):
     # Utiliza una distribución normal para el tiempo de falla (F(t))
-    return random.normalvariate(1,0.2)
-
+    return random.normalvariate(mu,sigma)
--- src/code/simulator.py
+++ src/code/simulator.py
@@ -1,17 +1,24 @@
 import random
-
+from code.factory_data_collector import FactoryDataCollector
 from src.code.factory import Factory
+import src.code.auxiliar_functions as aux
 
 # Todo: sacar el data collector a una clase
 # Todo: Manejar la data
 
+
 class Simulator:
-    def __init__(self,i:int =0,n:int =0,s:int =0,random_values:bool = True):
+    def __init__(self,i:int =0,n:int =0,s:int =0,lambd:float=aux.var_lambd,mu:float=aux.var_mu,sigma:float=aux.var_sigma,random_values:bool = True):
         self.__iterations = i
         self.__n = n
         self.__s = s
+        
+        aux.var_lambd = lambd
+        aux.var_mu = mu
+        aux.var_sigma = sigma
+        
         self.__random_values = random_values
-        self.__results = []
+        self.__results : list[FactoryDataCollector] = []
 
         if self.__random_values:
             self.__n = random.randint(10,50)
@@ -23,15 +30,17 @@ class Simulator:
         for i in range(self.__iterations):
             factory = Factory(self.__n,self.__s)
             factory.start_factory()
-            factory_time = factory.get_factory_time()
             factory_data = factory.get_data()
-            self.__results.append((factory_time,factory_data))
+            self.__results.append(factory_data)
+            
+            
             
     def get_results(self):
         return self.__results
     
     def show_results(self):
-        for (factory_time,factory_data) in self.__results:
-            print(f"Factory ran for {factory_time} seconds")
-            print(factory_data)
+        for factory_data in self.__results:
+            print(f"Factory ran for {factory_data.get_working_time()} seconds")
+            print(factory_data.get_logs())
             print("\n")
+            
